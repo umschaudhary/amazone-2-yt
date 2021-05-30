705812215147120c852d210b87240870920d93b1
Umesh Chaudhary <umesschaudhary@gmail.com>
2021-05-31 00:36:17 +0545

    add autoPlay on Carousel

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,25 +4,28 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 export default function Banner(){
 	return (
 		<div className="relative">
-		<div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-trasparent bottom-0 z-20" /> 
-			<Carousel
-				autoplay
-				infiniteLoop
-				showStatus={false}
-				showIndicators={false}
-				showThumbs={false}
-				interval={500}
-			>
-			<div>
-				<img loading="lazy" src="https://links.papareact.com/gi1" alt="image1" />
-			</div>
-			<div>
-				<img loading="lazy" src="https://links.papareact.com/6ff" alt="image1" />
-			</div>
-			<div>
-				<img loading="lazy" src="https://links.papareact.com/7ma" alt="image1" />
-			</div>
-			</Carousel>
+			<div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-trasparent bottom-0 z-20" /> 
+				<Carousel
+					autoPlay
+					infiniteLoop
+					showStatus={false}
+					showIndicators={false}
+					showThumbs={false}
+					interval={5000}
+				>
+					<div>
+						<img loading="lazy" src="https://links.papareact.com/gi1" alt="image1" />
+					</div>
+
+					<div>
+						<img loading="lazy" src="https://links.papareact.com/6ff" alt="image1" />
+					</div>
+
+					<div>
+						<img loading="lazy" src="https://links.papareact.com/7ma" alt="image1" />
+					</div>
+
+				</Carousel>
 		</div>
 	)
 
