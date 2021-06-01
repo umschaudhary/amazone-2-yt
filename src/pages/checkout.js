690f7e630fad89cc89f2b5c5ca0aaf423c1d66e7
Umesh Chaudhary <umesschaudhary@gmail.com>
2021-06-02 01:19:58 +0545

    add basket functionality

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,11 +1,15 @@
 import  Header from '../components/Header'
 import Image from 'next/image'
 import {useSelector} from 'react-redux'
-import {selectItems} from '../slices/basketSlice'
+import {selectItems, selectTotal} from '../slices/basketSlice'
 import CheckoutProduct from '../components/CheckoutProduct'
+import Currency from 'react-currency-formatter'
+import {useSession} from 'next-auth/client'
 
 export default function checkout() {
 	const items = useSelector(selectItems)
+	const total = useSelector(selectTotal)
+	const [session] = useSession()
 
 	return (
 	<div className="bg-gray-100">
@@ -43,7 +47,26 @@ export default function checkout() {
 	</div>
 
 			{/* right */}
-			<div></div>
+	<div className="flex flex-col bg-white p-10 shadow-md">
+		{items.length > 0 && (
+			<>
+			<h2 className="whitespace-nowrap">Subtotal ({
+				items.length
+			} items) :
+
+			<span className="font-bold">
+			<Currency quantity={total} currency="GBP" />
+			</span>
+			</h2>
+
+			<button 
+			disabled={!session}
+			className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 cursor-not-allowed'}`} >
+			{!session ? "Sigin to checkout" : "proceed to checkout"}
+			</button>
+			</>
+		)}
+	</div>
 			</main>
 		</div>
 	);
